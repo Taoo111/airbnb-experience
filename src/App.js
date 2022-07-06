aea510a0c2c97208b1034005feb4b91735b105ee
Dawid <dawidjura12@gmail.com>
2022-07-06 15:33:14 +0200

    Included data into card using props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Card from "./components/Card"
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
 
-export default function App(){
-    return(
-        <div className="container">
-            <Navbar />
-            <Hero />
-            <Card />
-        </div>
-    )
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+      <Hero />
+      <Card
+        image="katie.png"
+        rating="5.0"
+        review={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
+    </div>
+  );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,21 @@
-import playerPhoto from "../images/card-img-1.png";
 import star from "../images/star.png";
 
-export default function Card() {
+export default function Card(props) {
+  console.log(props);
+  const num = [1,2,3,4,5,6];
+  const doubleNum = num.map(num => num * 2);
+  console.log(doubleNum);
   return (
     <div className="card">
-      <img src={playerPhoto} alt="player" className="card--image"></img>
+      <img src={`images/${props.image}`} alt="player" className="card--image"></img>
       <div className="card--stats">
         <img src={star} alt="star" className="card--star"></img>
-        <span>5.0 </span>
-        <span className="gray"> (6) - </span>
-        <span className="gray"> USA</span>
+        <span>{props.rating}</span>
+        <span className="gray">({props.review}) - </span>
+        <span className="gray">{props.country}</span>
       </div>
-      <p>Life lessons with Katie Zaferes</p>
-      <p><span className="bold">From $126</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className="bold">From ${props.price}</span> / person</p>
     </div>
   );
 }
