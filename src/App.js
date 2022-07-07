072a50d67d84fe73b182662bd07ade955884efe1
Dawid <dawidjura12@gmail.com>
2022-07-07 14:05:27 +0200

    Added data file with informations included into cards

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
+import data from "./data";
 
 export default function App() {
+  const dataCards = data.map((item) => {
+    return (
+      <Card
+        coverImg={item.coverImg}
+        rating={item.stats.rating}
+        review={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+      />
+    );
+  });
   return (
     <div className="container">
       <Navbar />
       <Hero />
-      <Card
-        image="katie.png"
-        rating="5.0"
-        review={6}
-        country="USA"
-        title="Life lessons with Katie Zaferes"
-        price={136}
-      />
+      {dataCards}
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,14 @@
 import star from "../images/star.png";
 
 export default function Card(props) {
-  console.log(props);
-  const num = [1,2,3,4,5,6];
-  const doubleNum = num.map(num => num * 2);
-  console.log(doubleNum);
   return (
     <div className="card">
-      <img src={`images/${props.image}`} alt="player" className="card--image"></img>
+      <img src={`images/${props.coverImg}`} alt="player" className="card--image"></img>
       <div className="card--stats">
         <img src={star} alt="star" className="card--star"></img>
         <span>{props.rating}</span>
         <span className="gray">({props.review}) - </span>
-        <span className="gray">{props.country}</span>
+        <span className="gray">{props.location}</span>
       </div>
       <p>{props.title}</p>
       <p><span className="bold">From ${props.price}</span> / person</p>
